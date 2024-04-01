f2677e4d5042d37fe1cf7e01a6be4d8118c9560f
pinguille0027 <guillermo.orio27@gmail.com>
2024-04-01 15:58:07 +0200

    añadiendo mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [mdx()]
 });
--- src/api/comments.ts
+++ src/api/comments.ts
@@ -1,14 +1,15 @@
 export const prerender = false;
-
+import { getCollection } from 'astro:content';
 import { supabase } from "../lib/supabase";
 import type { APIRoute } from "astro";
 export const GET: APIRoute = async({request, params, props}) => {
+  const posts = await getCollection("proyecto");
+  console.log(posts)
   const { data, error } = await supabase
   .from('comentarios')
   .select()
   .eq("id_post", `${params.slug}`)
   console.log(params.slug)
-  console.log(data)
   if (error || !data) {
     return new Response(error.message, { status: 500 });
   }
