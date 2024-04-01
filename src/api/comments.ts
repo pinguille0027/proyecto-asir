41bbc562bd24c5ec9adfdb6b16449d072b207228
pinguille0027 <guillermo.orio27@gmail.com>
2024-04-01 23:38:28 +0200

    reestructuración de los resultados de la bbdd

--- src/api/comments.ts
+++ src/api/comments.ts
@@ -1,17 +1,42 @@
 export const prerender = false;
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
 import { supabase } from "../lib/supabase";
 import type { APIRoute } from "astro";
-export const GET: APIRoute = async({request, params, props}) => {
-  const posts = await getCollection("proyecto");
-  console.log(posts)
+export const GET: APIRoute = async ({ request, params, props }) => {
   const { data, error } = await supabase
-  .from('comentarios')
-  .select()
-  .eq("id_post", `${params.slug}`)
-  console.log(params.slug)
+    .from("comentarios")
+    .select()
+    .eq("id_post", `${params.slug}`);
+
   if (error || !data) {
     return new Response(error.message, { status: 500 });
   }
-  return new Response(JSON.stringify(data), { status: 200 });
+
+  const comentariosAgrupados:any = {};
+  // Recorremos el array de comentarios para agruparlos
+  data.forEach(comentario => {
+    if (comentario.id_ref) {
+      // Si el comentario tiene una referencia, lo agregamos como respuesta a su respectivo comentario padre
+      const comentarioPadre = comentariosAgrupados[comentario.id_ref];
+      if (comentarioPadre) {
+        // Si encontramos el comentario padre, agregamos esta respuesta a su arreglo de respuestas
+        if (!comentarioPadre.respuestas) {
+          comentarioPadre.respuestas = [];
+        }
+        comentarioPadre.respuestas.push(comentario);
+      } else {
+        // Si no encontramos el comentario padre, creamos un objeto para él y lo agregamos a comentariosAgrupados
+        comentariosAgrupados[comentario.id_ref] = { respuestas: [comentario] };
+      }
+    } else {
+      // Si el comentario no tiene una referencia, lo agregamos directamente a comentariosAgrupados
+      comentariosAgrupados[comentario.id] = comentario;
+    }
+  });
+  
+  const comentariosReestructurados = Object.values(comentariosAgrupados);
+  
+  console.log(comentariosReestructurados);
+
+  return new Response(JSON.stringify(comentariosReestructurados), { status: 200 });
 };
