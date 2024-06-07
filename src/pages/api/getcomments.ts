312b6ebf2d60e5182f9013f130f2b9a739feb8ab
pinguille0027 <guillermo.orio27@gmail.com>
2024-06-07 14:37:18 +0200

    moviendo la ruta de los comentarios

--- src/pages/api/getcomments.ts
+++ src/pages/api/getcomments.ts
@@ -1,6 +1,6 @@
-export const prerender = false;
+
 import { getCollection } from "astro:content";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../../lib/supabase";
 import type { APIRoute } from "astro";
 export const GET: APIRoute = async ({ request, params, props }) => {
   const { data, error } = await supabase
